0cd9361c8daaf8a372d29f73c1155bd60a08d9fd
JalenPrim <1265087@essextech.org>
2025-05-02 12:43:25 -0400

    Update colorChanger.js

--- colorChanger.js
+++ colorChanger.js
@@ -1,8 +1,8 @@
 let button = document.querySelector("colorButton");
 let box = document.querySelector("colorBox");
 
-box.addEventListener("click", function() {
-box.style.backgroundColor = "blue";
+button.addEventListener("click", function() {
+  box.style.backgroundColor = "blue";
 });
 
 
